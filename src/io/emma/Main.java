bf62aa181ce5445c54e03b972bcf78a25ea6008e
EmmaHogberg <70634183+EmmaHogberg@users.noreply.github.com>
2020-12-15 11:07:20 +0100

    Finished game

--- src/io/emma/Main.java
+++ src/io/emma/Main.java
@@ -1,10 +1,7 @@
 package io.emma;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -69,6 +66,13 @@ public class Main {
         return words[random.nextInt(words.length)];
     }
 
+    // Get the word and make it to arrays
+    static void getWordToArrays (String newWord) {
+        word = newWord;
+        correctWordArray = word.toCharArray();
+        shownWordArray = word.replaceAll("[A-Öa-ö]", "_").toCharArray();
+    }
+
     // Check if a correct character is given
     static boolean isCharacterAnswerCorrect() {
         boolean isGuessCorrect = false;
@@ -88,11 +92,13 @@ public class Main {
     static boolean isWordAnswerCorrect() {
         boolean isGuessCorrect = false;
 
-        if (inputWord != null && inputWord.equals(word)) {
-            isGuessCorrect = true;
-        }
         if(Arrays.equals(shownWordArray, correctWordArray)) {
             isGuessCorrect = true;
+        }
+
+        if (inputWord != null && inputWord.equals(word)) {
+            isGuessCorrect = true;
+            shownWordArray = correctWordArray;
         }
         return isGuessCorrect;
     }
@@ -100,151 +106,109 @@ public class Main {
     // Print bunny depending on how many tries the player have done
     static void printBunny() {
 
+        Scanner scanner = null;
         try {
-
-            switch (guessesLeft) {
-
-                case 0:
-                    Scanner scanner = new Scanner(new File("bunny0.txt"));
-                    while (scanner.hasNextLine())
-                        System.out.println(scanner.nextLine());
-                    break;
-
-                case 1:
-                    scanner = new Scanner(new File("bunny1.txt"));
-                    while (scanner.hasNextLine())
-                        System.out.println(scanner.nextLine());
-                    break;
-
-                case 2:
-                    scanner = new Scanner(new File("bunny2.txt"));
-                    while (scanner.hasNextLine())
-                        System.out.println(scanner.nextLine());
-                    break;
-
-                case 3:
-                    scanner = new Scanner(new File("bunny3.txt"));
-                    while (scanner.hasNextLine())
-                        System.out.println(scanner.nextLine());
-                    break;
-
-                case 4:
-                    scanner = new Scanner(new File("bunny4.txt"));
-                    while (scanner.hasNextLine())
-                        System.out.println(scanner.nextLine());
-                    break;
-
-                case 5:
-                    scanner = new Scanner(new File("bunny5.txt"));
-                    while (scanner.hasNextLine())
-                        System.out.println(scanner.nextLine());
-                    break;
-
-                case 6:
-                    scanner = new Scanner(new File("bunny6.txt"));
-                    while (scanner.hasNextLine())
-                        System.out.println(scanner.nextLine());
-                    break;
-
-                case 7:
-                    scanner = new Scanner(new File("bunny7.txt"));
-                    while (scanner.hasNextLine())
-                        System.out.println(scanner.nextLine());
-                    break;
-
-                case 8:
-                    scanner = new Scanner(new File("bunny8.txt"));
-                    while (scanner.hasNextLine())
-                        System.out.println(scanner.nextLine());
-                    break;
-            }
+            scanner = new Scanner(new File("bunny" + guessesLeft + ".txt"));
+            while (scanner.hasNextLine())
+                System.out.println(scanner.nextLine());
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
     }
-
 
 
     public static void main(String[] args) {
 
         boolean ongoingGame = true;
 
-        // Word-arrays
-        word = getRandomWord();
-        correctWordArray = word.toCharArray();
-        shownWordArray = word.replaceAll("[A-Öa-ö]", "_").toCharArray();
+        // Get new random word for this game
+        getWordToArrays(getRandomWord());
 
         // Correct answer
         System.out.println(correctWordArray);
 
         // Game intro
-        System.out.println("Välkommen till spelet där du får försöka gissa rätt ord för att rädda gubben från att hängas. Gissa ordet:");
+        System.out.println("Välkommen till spelet där du får försöka gissa rätt ord för att rädda kaninen från att uppslukas av marken. Gissa ordet:");
         System.out.println(shownWordArray);
 
         while (ongoingGame) {
 
             // Start menu with three options
-            System.out.println("");
-            System.out.println("||  Antal försök (a)  ||  Gissa bokstav (b)  ||  Gissa ordet (c)");
+            System.out.println("\n" + "||  Antal försök (a)  ||  Gissa bokstav (b)  ||  Gissa ordet (c)  ||  Eget ord (d)");
             System.out.print("Välj vad du vill göra: ");
             Scanner reader = new Scanner(System.in);
             String inputChoice = reader.nextLine();
 
-            // 1. Show number of completed guesses and remaining attempts
-            if(inputChoice.equals("a")) {
-                System.out.println("Antal försök: " + tries);
-                System.out.println("Återstående försök: " + guessesLeft);
-                printBunny();
-            }
+            switch (inputChoice) {
 
-            // 2. Guess a character
-            else if(inputChoice.equals("b")) {
-                tries++;
-                System.out.print("Skriv den bokstav du vill gissa på: ");
-                inputCharacter = reader.nextLine().charAt(0);
+                // 1. Show number of completed guesses and remaining attempts
+                case "a" -> {
+                    System.out.println("Antal försök: " + tries);
+                    System.out.println("Återstående försök: " + guessesLeft);
+                    printBunny();
+                }
 
-                if(isCharacterAnswerCorrect()) {
+                // 2. Guess a character
+                case "b" -> {
+                    tries++;
+                    System.out.print("Skriv den bokstav du vill gissa på: ");
+                    inputCharacter = reader.nextLine().charAt(0);
+                    if (isCharacterAnswerCorrect()) {
 
+                        if (isWordAnswerCorrect()) {
+                            System.out.println("\n" + "Grattis! Det var rätt ord och kaninen är räddad");
+                            guessesLeft = 8;
+                            ongoingGame = false;
+                        }
+                    }
+                    else {
+                        System.out.println("\n" + "Den bokstaven fanns inte med");
+                        guessesLeft--;
+                    }
+                    printBunny();
+                    System.out.println(shownWordArray);
+                }
+
+                // 3. Guess the whole word
+                case "c" -> {
+                    tries++;
+                    System.out.print("Skriv det ord som du vill gissa på: ");
+                    inputWord = reader.nextLine();
                     if (isWordAnswerCorrect()) {
-                        System.out.println("Grattis! Det var rätt ord och kaninen är räddad");
+                        System.out.println("\n" + "Grattis! Det var rätt ord och kaninen är räddad");
                         guessesLeft = 8;
                         ongoingGame = false;
                     }
+                    else {
+                        System.out.println("\n" + "Det var fel ord");
+                        guessesLeft--;
+                    }
+                    printBunny();
+                    System.out.println(shownWordArray);
                 }
-                else {
-                    System.out.println("Den bokstaven fanns inte med");
-                    guessesLeft--;
+
+                // The player get to choose a word and this word replaces the original word
+                case "d" -> {
+                    System.out.println("Skriv det ord som du vill att spelet ska använda: ");
+                    String playersWord = reader.nextLine();
+                    getWordToArrays(playersWord);
                 }
-                printBunny();
-                System.out.println(shownWordArray);
+                default -> System.out.println("Det blev fel, välj a, b eller c");
             }
 
-            // 3. Guess the whole word
-            else if(inputChoice.equals("c")) {
-                tries++;
-                System.out.print("Skriv det ord som du vill gissa på: ");
-                inputWord = reader.nextLine();
-
-                if (isWordAnswerCorrect()) {
-                    System.out.println("Grattis! Det var rätt ord och kaninen är räddad");
-                    guessesLeft = 8;
-                    ongoingGame = false;
-                }
-                else {
-                    System.out.println("Det var fel ord");
-                    guessesLeft--;
-                }
-                printBunny();
-                System.out.println(shownWordArray);
-            }
-            else {
-                System.out.println("Det blev fel, välj a, b eller c");
-            }
-
+            // End the game
             if(guessesLeft == 0) {
                 System.out.println("Game over, hejdå kaninen :( ");
+                System.out.print("Rätt svar var: ");
+                System.out.print(correctWordArray);
                 ongoingGame = false;
+                reader.close();
             }
         }
     }
